7c3948d14340f793e291a0f4b4b683bcf094dd8c
21cadams <21cadams@athenian.org>
2021-01-12 05:47:35 +0000

    Shuffler

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,17 +1,44 @@
+import java.util.Random;
 public class MyMain {
+    
 
     // Shuffles an array, using the perfect shuffle algorithm
     public static int[] perfectShuffle(int[] arr) { 
-        return null;
+        int leng= arr.length;
+        int[] array = new int[leng];
+        int first = 0;
+        int second = leng/2;
+        int count=0;
+        for (int i =0;i<leng/2; i++){
+            array[count] = arr[first];
+            array[count+1]=arr[second];
+            first++;
+            second++;
+            count=count+2;
+        }
+        return array;
     }
 
     // Shuffles an array, using the selection shuffle algorithm
     public static int[] selectionShuffle(int[] arr) { 
-        return null;
+        int leng= arr.length;
+        int[] array=new int[leng];
+        array=arr;
+        
+        for (int i=0; i<leng-1; i++){
+            int random =(int) (Math.random()*leng);
+            int holder = array[i];
+            array[i]=array[random];
+            array[random]=holder;
+
+        }
+        return array;
     }
 
-
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        int[] array = new int[]{ 1,2,3,4,5,6,7,8,9,10};
+        System.out.println(selectionShuffle(array));
+        System.out.println(perfectShuffle(array));
+        
     }
 }
